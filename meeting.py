866d254b41f336d4696efc384674f65a2cadb48f
shubham kumar <34011634+shubhamrouniyar@users.noreply.github.com>
2023-08-29 11:34:46 +0530

    Create meeting.py

---
+++ meeting.py
@@ -0,0 +1,8 @@
+class Meeting:
+    def __init__(self, id,  duration, participant, subject ):
+        self.id = id
+        self.duration = duration
+        self.participant = participant
+        self.subject = subject
+        
+    
